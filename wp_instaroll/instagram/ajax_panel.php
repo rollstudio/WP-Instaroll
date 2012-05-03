56124dbacec9b19e401920999b9cd093d7f33b3c
Marco Iannaccone <m.iannaccone@gmail.com>
2012-05-03 15:41:13 +0200

    reorganization of create post ajax action complete

--- wp_instaroll/instagram/ajax_panel.php
+++ wp_instaroll/instagram/ajax_panel.php
@@ -189,158 +189,20 @@ add_action('wp_ajax_wpinstaroll_photosbytagtable', 'instagram_photosbytagtable_a
 // handler for creating a post from Instagram pic
 function instagram_createpostfromphoto_ayax()
 {
-	if (!isset($_POST['url']) || !isset($_POST['id']) || !isset($_POST['link']))
+	$response = '';
+
+	if (!isset($_POST['id']) || !isset($_POST['url']) || !isset($_POST['link']))
 	{
 		$response = array(
 			'error' => true,
 			'error_description' => 'Required parameters missing'
 		);
-		print(json_encode($response));
-		
-		exit;
-	}
-	
-	
-	$search_tag = get_option(WP_ROLL_INSTAGRAM_PLUGIN_PREFIX.'_instagram_search_tag');
-	if (empty($search_tag))
-	{
-		$response = array(
-			'error' => true,
-			'error_description' => 'Instagram access not properly configured'
-		);
-		print(json_encode($response));
-		
-		exit;
-	}
-
-	$title_placeholder = get_option(WP_ROLL_INSTAGRAM_PLUGIN_PREFIX.'_instagram_post_title_placeholder');
-
-	// a. if the category corresponding to the Instagram search tags
-	// doesn't exist, we create it
-	$category_name = '#'.$search_tag;
-	$cat_id = category_exists($category_name);
-	if (!$cat_id)
-		$cat_id = wp_create_category($category_name);	
-	
-	
-	// b. post creation
-	$created_post_status = get_option(WP_ROLL_INSTAGRAM_PLUGIN_PREFIX.'_instagram_created_post_status');
-	if ($created_post_status != 'publish')
-		$created_post_status = 'draft';
-
-	$insert_photo_mode = get_option(WP_ROLL_INSTAGRAM_PLUGIN_PREFIX.'_instagram_insert_photo_mode');
-	if ($insert_photo_mode !== 'featured')
-		$insert_photo_mode = 'post_content';
-
-
-	$post_args = array(
-		'post_author' 	=> 0,
-		'post_category'	=> array($cat_id),
-		'post_content' 	=> $_POST['caption'],
-		'post_status'	=> $created_post_status, 
-		'post_title'	=> $title_placeholder,
-		'post_type'		=> 'post' 
-	);
-	$created_post_ID = wp_insert_post($post_args);
-	
-	if (!$created_post_ID)
-	{
-		$response = array(
-			'error' => true,
-			'error_description' => 'Problem creating the post'
-		);
-		print(json_encode($response));
-		
-		exit;
-	}
-
-
-	// c. add Instagram pic metadata to the just created post
-	update_post_meta($created_post_ID, '_'.WP_ROLL_INSTAGRAM_PLUGIN_METADATA_PREFIX.'_insta_id', $_POST['id']);
-	update_post_meta($created_post_ID, '_'.WP_ROLL_INSTAGRAM_PLUGIN_METADATA_PREFIX.'_insta_link', $_POST['link']);
-	update_post_meta($created_post_ID, '_'.WP_ROLL_INSTAGRAM_PLUGIN_METADATA_PREFIX.'_insta_authorusername', $_POST['author_username']);
-	update_post_meta($created_post_ID, '_'.WP_ROLL_INSTAGRAM_PLUGIN_METADATA_PREFIX.'_insta_authorid', $_POST['author_id']);	
-	
-	
-	// d. download image from Instagram and associate to post
-	$photo_data = getInstagramPhotoDataFromInstaID($_POST['id']);
-
-		// if we have already the image inside the media library, we get it from there, without actually downloading it from Instagram
-	$image_info = null;
-	if ($photo_data && $photo_data->media_id)
-	{
-		$image_info = wp_get_attachment_image_src($photo_data->media_id, 'full');
-	}
-
-	if (!$image_info)
-	{
-		$tmp = download_url($_POST['url']);
-	    $file_array = array(
-	        'name' => basename($_POST['url']),
-	        'tmp_name' => $tmp
-	    );
-
-	    if (is_wp_error($tmp))
-		{
-			@unlink($file_array['tmp_name']);
-			
-			$response = array(
-				'error' => true,
-				'error_description' => 'Problem downloading the image from Instagram'
-			);
-			print(json_encode($response));
-			
-			exit;
-	    }
-
-	    $attach_id = media_handle_sideload($file_array, $created_post_ID);
-	    if (is_wp_error($attach_id))
-		{
-	        @unlink($file_array['tmp_name']);
-	        
-			$response = array(
-				'error' => true,
-				'error_description' => 'problem adding the image to the post'
-			);
-			print(json_encode($response));
-	    }
-		
-		@unlink($file_array['tmp_name']);
 	}
 	else
-		$attach_id = $photo_data->media_id;
+		$response = wpinstaroll_createpostfromphoto($_POST['id'], $_POST['url'], $_POST['link'], $_POST['caption'], $_POST['author_username'], $_POST['author_id']);
 
-	
-	if ($insert_photo_mode === 'featured')
-	{
-		// attach to image as featured image (post thumbnail)
-		add_post_meta($created_post_ID, '_thumbnail_id', $attach_id, true);
-	}
-	else {
-
-		if (!$image_info)
-			$image_info = wp_get_attachment_image_src($attach_id, 'full');
-
-		// insert the image inside the post, followed by post caption
-		$update_post_data = array();
-  		$update_post_data['ID'] = $created_post_ID;
-  		$update_post_data['post_content'] = '<img src="'.$image_info[0].'" alt="'.strip_tags($_POST['caption']).'" width="'.$image_info[1].'" height="'.$image_info[2].'"/><br/>'.
-  											$_POST['caption'];
-
-  		wp_update_post($update_post_data);
-	}
-
-
-	// update Instagram photo local data
-	updateInstagramPhotoStatus($_POST['id'], true, $attach_id);
-
-	
-	$response = array(
-		'error' => false,
-		'post_id' => $created_post_ID
-	);
-	print(json_encode($response));
-
+	echo json_encode($response);
+		
 	exit;
 	
 	// accessible with URL:
